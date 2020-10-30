c1ea6c4f2c03b90376f3a86d1ca4746ef7c14bdf
AmandineLabaeye <amandine.labaeye@orange.fr>
2020-10-30 15:52:59 +0100

    Création & Ajout de l'évèvenement pour les moteurs

--- src/com/company/Motor.java
+++ src/com/company/Motor.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import org.fusesource.jansi.Ansi;
+
 import java.util.*;
 
 /**
- * 
+ *
  */
 public class Motor extends Mecanic {
+
+    private int[] NombreGenererAlea;
 
     /**
      * Default constructor
@@ -18,11 +22,38 @@ public class Motor extends Mecanic {
         repairMotor();
     }
 
+    private int random(int min, int max) {
+        Random random = new Random();
+        return min + random.nextInt(max - min + 1);
+    }
+
     /**
-     * 
+     *
      */
     public void repairMotor() {
-        // TODO implement here
+        // Afficher 5 chiffres Aléatoire de 0 à 9 et vérifier qu'il recopie bien correctement la suite si faux,
+        // il recommence jusqu'à ce qu'il la raison pour pouvoir réparer son moteur
+
+        System.out.println("Une fumée sort de votre moteur, vous devez le réparer en réecrivant correctement la suite" +
+                " de 5 chiffres qui va apparaître");
+
+        int min = 10000;
+        int max = 99999;
+        int result = random(min, max);
+
+        System.out.println("La suite de 5 chiffres : " + result);
+
+        Scanner Scanner = new Scanner(System.in);
+
+        String NombreAlea = Scanner.nextLine();
+
+        if (NombreAlea.equals(result)) {
+            System.out.println("Votre moteur est réparée vous pouvez repartir");
+        } else {
+            System.out.println("Ce n'est pas la bonne suite réessaye encore " + result);
+            NombreAlea = Scanner.nextLine();
+        }
     }
+
 
 }
